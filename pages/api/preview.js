5f78365d7008fd5716d6c216773f522ca12cb972
Henry Smith <henry.smith@netlify.com>
2023-02-15 16:13:08 +0000

    Update to set location header

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -10,7 +10,9 @@ export default async (req, res) => {
 
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  console.log("req.query.slug", req.query.slug);
-  res.query = null;
-  res.redirect(`/${req.query.slug}`);
+  //   console.log("res", res.setHeader("Location", req.query.slug));
+  //   //  res.headers.set("Location", req.query.slug);
+
+  res.setHeader("Location", `https://${req.headers.host}/${req.query.slug}`);
+  return res.status(307).json({});
 };
